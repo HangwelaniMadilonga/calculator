f70d4f7e9e02e34b7a26a4f0f36111c6faf9d480
Hangwelani Madilonga <hangwelani3@gmail.com>
2023-11-02 13:20:42 +0200

    Change font to make it more intuituve.
    Add clear feature.

--- calculator.js
+++ calculator.js
@@ -1,5 +1,10 @@
+let firstValue = 0;
+let secondValue = 0;
+let calculationResult = 0;
+
 let buttons = document.querySelectorAll('.calculator-button');
 let calculatorQuestion = document.querySelector('#calculations-asked');
+let calculatorAnswer = document.querySelector('#calculations-answer');
 
 for (let button of buttons) {
     button.addEventListener('click', function () {
@@ -8,9 +13,18 @@ for (let button of buttons) {
         setTimeout(() => {
             button.classList.remove('animation-add');
         },500)
+        button.textContent = button.textContent.replace(/\s+/g, "");
+        
+        if (button.textContent === "AC"){
+            calculatorQuestion.textContent = "";
+            calculatorAnswer.textContent = "";
+        }
+
+        else{
         
         calculatorQuestion.textContent = calculatorQuestion.textContent + button.textContent;
-        calculatorQuestion.textContent  =  calculatorQuestion.textContent .replace(/\s+/g, "");
+        // calculatorQuestion.textContent  =  calculatorQuestion.textContent .replace(/\s+/g, "");
+    }
         
     })
 }
